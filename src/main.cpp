1614b366873c3937f8243adcc2b54addca4ca541
wynss <toniaxelsson@icloud.com>
2016-05-24 09:07:44 +0200

    Added FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <chrono>
 
 #include "Processor.hpp"
 #include "CameraHandler.hpp"
@@ -8,7 +9,13 @@ WQueue<cv::Mat> rawQueue;
 WQueue<cv::Mat> procQueue;
 bool capture = true;
 
+double avgFPS = 0.0; // Holds the average FPS
+int fps1sec = 0; // Counter for number of frames during previous second
+std::chrono::system_clock::time_point start; // Time used when calculating FPS
+
 void startCapture(CameraHandler&);
+void updateAvgFPS();
+std::string getFPSString(void);
 
 int main(int argc, const char* argv[]) {
     
@@ -30,23 +37,30 @@ int main(int argc, const char* argv[]) {
     Processor processor(rawQueue, procQueue);
     std::thread procThread(&Processor::startProcessing, &processor);
     
+    // Get start time
+    start = std::chrono::system_clock::now();
+    
     // Start GUI loop
     while(capture) {
 		
+        updateAvgFPS();
+        
         // Grab frame from processed frames queue
 		procQueue.popItem(frame);
 		
         // Add some text
+        cv::putText(frame, getFPSString(), cv::Point(5,20),cv::FONT_HERSHEY_DUPLEX, 0.5, cv::Scalar(0,255,0));
+        
         cv::putText(frame,
                     "RQueue: " + std::to_string(rawQueue.numItems()),
-                    cv::Point(5,20),
+                    cv::Point(5,40),
                     CV_FONT_HERSHEY_DUPLEX,
                     0.6,
                     cv::Scalar(255,0,0));
         
         cv::putText(frame,
                     "RQueue: " + std::to_string(procQueue.numItems()),
-                    cv::Point(5,40),
+                    cv::Point(5,60),
                     CV_FONT_HERSHEY_DUPLEX,
                     0.6,
                     cv::Scalar(255,0,0));
@@ -81,3 +95,26 @@ void startCapture(CameraHandler &ch) {
 		rawQueue.pushItem(capFrame);
 	}
 }
+
+// Calculates the average FPS
+void updateAvgFPS()
+{
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    if(diff.count() > 1.0)
+    {
+        start = std::chrono::system_clock::now();
+        avgFPS=0.3*avgFPS+0.7*fps1sec;
+        fps1sec=0;
+    }
+    fps1sec++;
+}
+
+// Returns a string containing the average FPS
+std::string getFPSString()
+{
+    char buffer [20];
+    sprintf(buffer, "FPS %4.1f", avgFPS);
+    return std::string(buffer);
+}
+
